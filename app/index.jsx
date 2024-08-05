ba240df68ba17103ff709ad98ac2f3a0bded79a8
Hanif1201 <adeyemihanif@gmail.com>
2024-08-05 14:48:42 +0100

    set up

---
+++ app/index.jsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Page() {
+  return (
+    <View className='bg-black flex'>
+      <Text>Hello there</Text>
+    </View>
+  );
+}
