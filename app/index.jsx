756edadc0a40e0cf795c9b479b72bc87b902fea2
Hanif1201 <adeyemihanif@gmail.com>
2024-08-05 15:02:23 +0100

    update

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,41 @@
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
 
-export default function Page() {
+import { StatusBar } from "expo-status-bar";
+import { Redirect, router } from "expo-router";
+import { View, Text, Image, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
+
+import { images } from "../constants";
+
+const Welcome = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.navigate("onboarding"); // Replace 'HomePage' with the name of the page you want to navigate to
+    }, 3000); // 3000 milliseconds = 3 seconds
+
+    return () => clearTimeout(timer); // Cleanup the timer when the component unmounts
+  }, [navigation]);
   return (
-    <View className='bg-black flex'>
-      <Text>Hello there</Text>
-    </View>
+    <SafeAreaView className='bg-white h-full'>
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className='w-full flex justify-center items-center h-full px-4'>
+          <Image
+            source={images.logo}
+            className='w-100% '
+            resizeMode='contain'
+          />
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor='#161622' style='light' />
+    </SafeAreaView>
   );
-}
+};
+
+export default Welcome;
---
+++ constants/images.js
@@ -0,0 +1,4 @@
+import logo from "../assets/images/SUNUTractor.png";
+import tractor from "../assets/images/tractor.png";
+
+export default { logo, tractor };
