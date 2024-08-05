108bf6523fddbbd0e86fb696c8b75e735f923dc3
Hanif1201 <adeyemihanif@gmail.com>
2024-08-05 22:38:17 +0100

    auth pages

---
+++ app/(auth)/sign-in.jsx
@@ -0,0 +1,125 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  Dimensions,
+} from "react-native";
+import { Link, router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
+import React, { useState } from "react";
+import { images } from "../../constants";
+
+const signIn = () => {
+  // const { setUser, setIsLogged } = useGlobalContext();
+  const [isSubmitting, setSubmitting] = useState(false);
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const submit = async () => {
+    if (form.email === "" || form.password === "") {
+      Alert.alert("Error", "Please fill in all fields");
+    }
+
+    setSubmitting(true);
+
+    try {
+      await signIn(form.email, form.password);
+      const result = await getCurrentUser();
+      setUser(result);
+      setIsLogged(true);
+
+      Alert.alert("Success", "User signed in successfully");
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <SafeAreaView className='bg-white h-full '>
+      <ScrollView
+        contentContainerStyle={styles.scrollViewContent}
+        keyboardShouldPersistTaps='handled'
+      >
+        <View
+          className='w-full flex justify-center h-full px-4 my-6'
+          style={{
+            minHeight: Dimensions.get("window").height - 100,
+          }}
+        >
+          <Image
+            source={images.logo}
+            resizeMode='contain'
+            className='w-[300px] h-[200px] -mt-20'
+          />
+          <Text className='text-xl font-semibold text-black   text-center font-psemibold'>
+            Welcome back!
+          </Text>
+          <Text className='text-base font-medium text-black  text-center font-pmedium'>
+            Log in to your exisitent account
+          </Text>
+
+          <FormField
+            title='Email'
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles='mt-7'
+            keyboardType='email-address'
+          />
+
+          <FormField
+            title='Password'
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles='mt-7'
+          />
+
+          <CustomButton
+            title='Sign In'
+            handlePress={submit}
+            containerStyles='mt-7'
+            isLoading={isSubmitting}
+          />
+
+          <View className='flex justify-center pt-5 flex-row gap-2'>
+            <Text className='text-lg text-black font-pregular'>
+              Don't have an account?
+            </Text>
+            <Link
+              href='/sign-up'
+              className='text-lg font-psemibold text-secondary'
+            >
+              Signup
+            </Link>
+          </View>
+
+          <Link href='/home' className='text-lg font-psemibold text-secondary'>
+            Home
+          </Link>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollViewContent: {
+    flexGrow: 1,
+  },
+  container: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: Dimensions.get("window").height,
+  },
+});
+
+export default signIn;
---
+++ app/(auth)/sign-up.jsx
@@ -0,0 +1,121 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  Dimensions,
+} from "react-native";
+import { Link, router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
+import React, { useState } from "react";
+import { images } from "../../constants";
+
+const signUp = () => {
+  const [isSubmitting, setSubmitting] = useState(false);
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const submit = async () => {
+    if (form.username === "" || form.email === "" || form.password === "") {
+      Alert.alert("Error", "Please fill in all fields");
+    }
+
+    setSubmitting(true);
+    try {
+      const result = await createUser(form.email, form.password, form.username);
+      setUser(result);
+      setIsLogged(true);
+
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+  return (
+    <SafeAreaView className='bg-white h-full '>
+      <ScrollView
+        contentContainerStyle={styles.scrollViewContent}
+        keyboardShouldPersistTaps='handled'
+      >
+        <View
+          className='w-full flex justify-center h-full px-4 my-6'
+          style={{
+            minHeight: Dimensions.get("window").height - 100,
+          }}
+        >
+          <Image
+            source={images.logo}
+            resizeMode='contain'
+            className='w-[300px] h-[200px] -mt-20'
+          />
+          <Text className='text-xl font-semibold text-black   text-center font-psemibold'>
+            Lets Get Started!
+          </Text>
+          <Text className='text-base font-medium text-black  text-center font-pmedium'>
+            Create an account to get all features
+          </Text>
+          <FormField
+            title='Username'
+            value={form.username}
+            handleChangeText={(e) => setForm({ ...form, username: e })}
+            otherStyles=''
+          />
+          <FormField
+            title='Email'
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles='mt-7'
+            keyboardType='email-address'
+          />
+
+          <FormField
+            title='Password'
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles='mt-7'
+          />
+
+          <CustomButton
+            title='Sign Up'
+            handlePress={submit}
+            containerStyles='mt-7'
+            isLoading={isSubmitting}
+          />
+
+          <View className='flex justify-center pt-5 flex-row gap-2'>
+            <Text className='text-lg text-black font-pregular'>
+              Have an account already?
+            </Text>
+            <Link
+              href='/sign-in'
+              className='text-lg font-psemibold text-secondary'
+            >
+              Login
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollViewContent: {
+    flexGrow: 1,
+  },
+  container: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: Dimensions.get("window").height,
+  },
+});
+
+export default signUp;
