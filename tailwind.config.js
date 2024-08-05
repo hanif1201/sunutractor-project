42e0b4f441dc73a94a188f4ccf6242a326c836e7
Hanif1201 <adeyemihanif@gmail.com>
2024-08-05 15:17:52 +0100

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#336431",
+        secondary: "#A0CA49",
+        danger: "#FF4C4C",
+      },
+      fontFamily: {
+        pthin: ["Poppins-Thin", "sans-serif"],
+        pextralight: ["Poppins-ExtraLight", "sans-serif"],
+        plight: ["Poppins-Light", "sans-serif"],
+        pregular: ["Poppins-Regular", "sans-serif"],
+        pmedium: ["Poppins-Medium", "sans-serif"],
+        psemibold: ["Poppins-SemiBold", "sans-serif"],
+        pbold: ["Poppins-Bold", "sans-serif"],
+        pextrabold: ["Poppins-ExtraBold", "sans-serif"],
+        pblack: ["Poppins-Black", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
